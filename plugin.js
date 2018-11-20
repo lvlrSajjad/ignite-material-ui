8b3839a3370838223ecde59dbd60263d4b803555
Sajjad Asadi <lvlr.xaus@gmail.com>
2018-11-20 17:14:56 +0330

    now adds typescript support for ignite andross

--- plugin.js
+++ plugin.js
@@ -7,11 +7,33 @@ const add = async function (context) {
   const { ignite, print } = context
 
   // install a npm module and link it
+  await ignite.addModule('typescript', {dev: true })
+  await ignite.addModule('react-native-typescript-transformer', { dev: true })
+  await ignite.addModule('@types/react', { dev: true })
+  await ignite.addModule('@types/react-native', { dev: true })
   await ignite.addModule('react-native-material-textfield', { version: '0.12.0', link: true })
-  await ignite.addModule('react-native-vector-icons', { version: '6.1.0', link: true })
+  await ignite.addModule('react-native-vector-icons', { version: '4.4.2', link: true })
   await ignite.addModule('react-native-typescript-material-ui-collection', { version: '0.0.30', link: true })
   await ignite.addModule('react-native-material-ripple', { version: '0.8.0', link: true })
+  const copyJobs = [{
+    template: 'jsconfig.json',
+    target: `jsconfig.json`
+  },{
+    template: 'rn-cli.config.js',
+    target: `rn-cli.config.js`
+  }, {
+    template: 'tsconfig.json',
+    target: `tsconfig.json`
+  }, {
+    template: 'tslint.json',
+    target: `tslint.json`
+  }, {
+    template: 'typings.d.ts',
+    target: `typings.d.ts`
+  }]
 
+  // make the templates and pass in props with the third argument here
+  await ignite.copyBatch(context, copyJobs)
   // add our component example to the plugin component examples screen
  // await ignite.addPluginComponentExample(EXAMPLE_FILE, { title: 'Maps Example' })
 
@@ -27,6 +49,10 @@ const remove = async function (context) {
   const { ignite } = context
 
   // remove the npm module and unlink it
+  await ignite.removeModule('typescript', {dev: true })
+  await ignite.removeModule('react-native-typescript-transformer', { dev: true })
+  await ignite.removeModule('@types/react', { dev: true })
+  await ignite.removeModule('@types/react-native', { dev: true })
   await ignite.removeModule('react-native-material-textfield', { unlink: true })
   await ignite.removeModule('react-native-vector-icons', { unlink: true })
   await ignite.removeModule('react-native-typescript-material-ui-collection', { unlink: true })
